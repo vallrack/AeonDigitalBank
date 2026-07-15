9199ae8363cf5021ca263489b66c6ef59f3a2a71
Vallrack <vallrack67@gmail.com>
2026-07-15 14:01:34 -0500

    Fix TypeError on registration by providing Firebase storage context correctly

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -6,10 +6,10 @@ import { initializeFirebase } from './index';
 import { FirebaseProvider } from './provider';
 
 export function FirebaseClientProvider({ children }: { children: React.ReactNode }) {
-  const { app, db, auth, functions } = useMemo(() => initializeFirebase(), []);
+  const { app, db, auth, functions, storage } = useMemo(() => initializeFirebase(), []);
 
   return (
-    <FirebaseProvider app={app} db={db} auth={auth} functions={functions}>
+    <FirebaseProvider app={app} db={db} auth={auth} functions={functions} storage={storage}>
       {children}
     </FirebaseProvider>
   );
